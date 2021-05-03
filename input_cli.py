f3b0f2b14287f36fd69cac8d4f022cce4b958045
Maxime Princelle <maxime@princelle.org>
2021-05-03 23:15:33 +0200

    feat(input): end of ask for identifiants

--- input_cli.py
+++ input_cli.py
@@ -4,8 +4,62 @@
 
 # Main libraries
 import questionary
+import re
+
+
+class ImapLoginInfo:
+	email = ""
+	password = ""
+	server = ""
+
+
+regex_email = '^(\w|\.|\_|\-)+[@](\w|\_|\-|\.)+[.]\w{2,3}$'
+regex_url = "[a-zA-Z0-9@:%._\\+~#?&//=]{2,256}\\.[a-z]" + "{2,6}\\b([-a-zA-Z0-9@:%._\\+~#?&//=]*)"
 
 
 def input_cli_start():
-	first_name = questionary.text("What's your first name?").ask()
-	return first_name
+	imap_login = ImapLoginInfo()
+	print("\nPlease enter your login info for IMAP connection...")
+
+	imap_login.email = questionary.text(
+			"What's your email address?",
+			default=imap_login.email,
+			validate=lambda a: (
+				True if (len(a) > 0 and re.search(regex_email, a)) else "You must enter an email address.")
+		).ask() \
+		or ""
+
+	imap_login.password = questionary.password(
+			"What's the password of your mailbox? (don't worry we don't keep it)",
+			default=imap_login.password,
+			validate=lambda a: (
+				True if (len(a) > 2) else "You must enter a real password...")
+		).ask() \
+		or ""
+
+	imap_login.server = questionary.text(
+			"What's the server address of your mailbox? (IMAP URL)",
+			default=imap_login.server,
+			validate=lambda a: (
+				True if (len(a) > 0 and re.search(regex_url, a)) else "You must enter a server URL.."
+		)).ask() \
+		or ""
+
+	print("\nHere's your login info: ")
+	print(f"Your email address is: '{imap_login.email}'.")
+	print(
+		f"Your password is '{imap_login.password[:2]}...' no just kidding - "
+		f"I'm not going to tell anyone. 🤫"
+	)
+	print(f"Your mailbox IMAP server is: {imap_login.server}.")
+
+	print()
+	confirmation = questionary.confirm("is this correct?").ask()
+
+	if confirmation:
+		print("That is amazing! - Let's continue 💥🚀")
+	else:
+		print("That is unfortunate 🐡 - Well... Let's start again!")
+		return input_cli_start()
+
+	return imap_login
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # python3
 # main.py
 # Entrypoint for tensorflow-ai project
+# Created by Maxime Princelle
 # --------------------------------------
 
 # Main libraries
@@ -11,12 +12,18 @@ from input_cli import input_cli_start
 
 
 def main():
+    print("Email classification program powered by Tensorflow.")
+    print("Created by Maxime Princelle")
+
     # No input mode?
     imap_login = input_cli_start()
 
-    print("Hello World!")
-
-    print("Your first name is: " + imap_login)
+    print(f"Your email address is: '{imap_login.email}'.")
+    print(
+        f"Your password is '{imap_login.password[:2]}...' no just kidding - "
+        f"I'm not going to tell anyone. 🤫"
+    )
+    print(f"Your mailbox IMAP server is: {imap_login.server}.")
 
 
 if __name__ == "__main__":
