2512060a910c506746bf3922877184217c02d7ad
Maxime Princelle <maxime@princelle.org>
2021-05-19 20:42:39 +0200

    feature(classifier): adding classifier

---
+++ classifier.py
@@ -0,0 +1,69 @@
+# python3
+# classifier.py
+# Machine Learning processing for classifier
+# Created by Maxime Princelle
+# --------------------------------------
+import time
+
+import numpy as np
+import pandas as pd
+from sklearn.cluster import KMeans
+from sklearn.feature_extraction.text import TfidfVectorizer, ENGLISH_STOP_WORDS
+from sklearn.metrics.pairwise import linear_kernel
+from sklearn.preprocessing import normalize
+
+
+def top_tfidf_feats(row, features, top_n=20):
+	topn_ids = np.argsort(row)[::-1][:top_n]
+	top_feats = [(features[i], row[i]) for i in topn_ids]
+	df = pd.DataFrame(top_feats, columns=['features', 'score'])
+	return df
+
+
+def top_feats_in_doc(X, features, row_id, top_n=25):
+	row = np.squeeze(X[row_id].toarray())
+	return top_tfidf_feats(row, features, top_n)
+
+
+def classifier(mails_df):
+	mails_df_copy = mails_df
+
+	# Get stopwords
+	stopwords_txt_fr = open("stopwords_fr.txt", "r")
+	stopwords_fr = stopwords_txt_fr.read().split('\n')
+	stopwords = ENGLISH_STOP_WORDS.union(stopwords_fr)
+	#print(stopwords)
+
+	# Remove newlines
+	mails_df['text'].replace({r'\s+$': '', r'^\s+': ''}, regex=True, inplace=True)
+	mails_df['text'].replace(r'\n',  ' ', regex=True, inplace=True)
+	mails_df['text'].replace(r'\r', ' ', regex=True, inplace=True)
+
+	#print(mails_df.text)
+
+	vec = TfidfVectorizer(analyzer='word', stop_words=stopwords, max_df=0.3, min_df=3, max_features=30, token_pattern=r'(?u)\b[A-Za-z]+\b')
+	vec_train = vec.fit_transform(mails_df["text"])
+
+	features = vec.get_feature_names()
+	#print(features)
+	categories = top_feats_in_doc(vec_train, features, 1, 100)
+
+	#print(categories)
+	categorised_mails = {}
+
+	for idx, category in enumerate(categories.features):
+		vec_query = vec.transform([category])
+		cosine_sim = linear_kernel(vec_query, vec_train).flatten()
+		related_email_indices = cosine_sim.argsort()
+		category_emails = related_email_indices.tolist()
+
+		# Remove first element
+		category_emails.pop(0)
+
+		emails = category_emails
+		for idx, email_id in enumerate(category_emails):
+			emails[idx] = mails_df.iloc[email_id]
+
+		categorised_mails[category] = emails
+
+	return categorised_mails
--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 # python3
 # main.py
-# Entrypoint for tensorflow-ai project
+# Entrypoint for classifier-ai project
 # Created by Maxime Princelle
 # --------------------------------------
 
-# Main libraries
-
-# Import IMAP library
 import pandas as pd
+from classifier import classifier
 from imap.imap import IMAP as imap
 from input_cli import input_cli_start, ImapLoginInfo
 from dotenv import load_dotenv
@@ -20,13 +18,22 @@ def main(login_info=None):
 
 	# Init variables
 	mails = None
+	imap_login = ImapLoginInfo()
 
 	# Load .env or parameter
-	load_dotenv(".env")
-	imap_login = ImapLoginInfo()
-	imap_login.email = os.getenv("MAIL_ADDRESS") or login_info.email
-	imap_login.password = os.getenv("MAIL_PASS") or login_info.password
-	imap_login.server = os.getenv("MAIL_SERVER") or login_info.server
+	if os.path.exists("./.env"):
+		print("Load .env file.")
+		load_dotenv(".env")
+
+	if ("MAIL_ADDRESS" in os.environ) and ("MAIL_PASS" in os.environ) and ("MAIL_SERVER" in os.environ):
+		imap_login.email = os.getenv("MAIL_ADDRESS")
+		imap_login.password = os.getenv("MAIL_PASS")
+		imap_login.server = os.getenv("MAIL_SERVER")
+	else:
+		if login_info is not None:
+			imap_login.email = login_info['email']
+			imap_login.password = login_info['password']
+			imap_login.server = login_info['server']
 
 	if imap_login.email == "" or imap_login.password == "" or imap_login.server == "":
 		login_info = False
@@ -50,22 +57,32 @@ def main(login_info=None):
 
 		# Check if export file
 		if len(sys.argv) > 1 and sys.argv[1] == "-i":
-			mails = pd.read_feather(open(f"./export_{imap_login.email}.feather", 'r'))
+			print(f"-i detected, importing data from ./export_{imap_login.email}.pickle")
+			mails = pd.read_pickle(f"./export_{imap_login.email}.pickle")
 		else:
-			print("\nRetrieve mails from INBOX...")
+			print("\nRetrieve mails from INBOX (takes a looong time...)")
 			mailbox = imap(imap_login.email, imap_login.password, imap_login.server)
 			mailbox.connect()
 
 			mails = mailbox.fetch_mails_from_inbox()
 
-		print(mails)
+		# print(mails)
 
 		# If -e then export data.
 		if len(sys.argv) > 1 and sys.argv[1] == "-e":
-			print(f"-e detected, exporting data to ./export_{imap_login.email}.feather")
-			if os.path.exists(f"./export_{imap_login.email}.feather"):
-				os.remove(f"./export_{imap_login.email}.feather")
-			mails.to_feather(f"./export_{imap_login.email}.feather")
+			print(f"-e detected, exporting data to ./export_{imap_login.email}.pickle")
+			if os.path.exists(f"./export_{imap_login.email}.pickle"):
+				os.remove(f"./export_{imap_login.email}.pickle")
+			mails.to_pickle(f"./export_{imap_login.email}.pickle")
+
+		# ML Processing
+		categorised_mails = classifier(mails)
+
+		print("\nCategories\n")
+		print(categorised_mails.keys())
+
+		print("\nDone.")
+		return categorised_mails
 
 	else:
 		print("Error on getting mailbox login info.")
